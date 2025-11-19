7347d61586dd28b710490d9cf6a58f80de260cc6
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-11-18 20:48:33 -0800

    exercise is done

--- file_reader.js
+++ file_reader.js
@@ -2,4 +2,12 @@ const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
   // your code here
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        let arrayHobbies = data4.split(",")
+        console.log(`${data1} ${data2} is ${data3} years old and his hoobies are ${arrayHobbies[0].replace("[","")} and ${arrayHobbies[1].replace("]","")}`)
+      })
+    })
+  })
 })
